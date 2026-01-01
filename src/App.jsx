b74aae6ab80cfd4d5c9041126905e8dc89b6d570
mohaiminul69 <mohaiminul69@gmail.com>
2026-01-01 14:46:19 +0600

    react router added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import { Routes } from "react-router";
+import { Route } from "react-router";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<div>Test</div>} />
+    </Routes>
   );
 }
 
