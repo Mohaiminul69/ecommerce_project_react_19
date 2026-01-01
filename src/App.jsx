3c6096a571dc07942a79c6d0745a3a44dfa2e192
mohaiminul69 <mohaiminul69@gmail.com>
2026-01-01 15:17:48 +0600

    checkout page added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import "./App.css";
-import HomePage from "./pages/HomePage";
 import { Routes } from "react-router";
 import { Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import CheckoutPage from "./pages/CheckoutPage";
+import "./App.css";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<div>Test</div>} />
+      <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
   );
 }
